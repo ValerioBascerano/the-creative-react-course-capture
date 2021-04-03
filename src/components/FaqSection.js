fd1647fe82ff43b35c462aa41b4f0d5bb157b18b
Valerio Bascerano <valerio.bascerano@sistinf.it>
2021-04-04 00:38:09 +0200

    4. Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Qualunque domanda <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>Da dove iniziare?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure,
+            optio.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Programma quotidiano</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure,
+            optio.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Metodi di pagamento differenti</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure,
+            optio.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Che prodotti offrite?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure,
+            optio.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
